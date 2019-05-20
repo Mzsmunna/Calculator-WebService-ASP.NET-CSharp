b8d5c947c9a3343f968cbf092df17bb01f679a14
Mamunuz Zaman <mzs.munna@gmail.com>
2019-05-20 17:00:01 +0600

    Add files via upload

---
+++ WebCalculator/Calculator.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="WebCalculator.Calculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #form1 {
+            height: 420px;
+            width: 246px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="resultBox" runat="server" Height="36px" Width="243px"></asp:TextBox>
+        </div>
+        <asp:Button ID="ButtonPer" runat="server" OnClick="ButtonPer_Click" Text="%" />
+        <asp:Button ID="ButtonRoot" runat="server" OnClick="ButtonRoot_Click" Text="√" />
+        <asp:Button ID="ButtonSqr" runat="server" OnClick="ButtonSqr_Click" Text="x^2" />
+        <asp:Button ID="ButtonDiv" runat="server" OnClick="ButtonDiv_Click" Text="/" />
+        <asp:Button ID="ButtonMult" runat="server" OnClick="ButtonMult_Click" Text="x" />
+        <asp:Button ID="ButtonSum" runat="server" OnClick="ButtonSum_Click" Text="+" />
+        <asp:Button ID="ButtonSub" runat="server" OnClick="ButtonSub_Click" Text="-" />
+        <asp:Button ID="ButtonDott" runat="server" OnClick="ButtonDott_Click" Text="." />
+        <asp:Button ID="ButtonEqual" runat="server" OnClick="ButtonEqual_Click" Text="=" />
+        <asp:Button ID="ButtonClear" runat="server" OnClick="ButtonClear_Click" Text="Clear" />
+        <asp:Button ID="Button09" runat="server" OnClick="Button09_Click" Text="9" />
+        <asp:Button ID="Button08" runat="server" OnClick="Button08_Click" Text="8" />
+        <asp:Button ID="Button07" runat="server" OnClick="Button07_Click" Text="7" />
+        <asp:Button ID="Button06" runat="server" OnClick="Button06_Click" Text="6" />
+        <asp:Button ID="Button05" runat="server" OnClick="Button05_Click" Text="5" />
+        <asp:Button ID="Button04" runat="server" OnClick="Button04_Click" Text="4" />
+        <asp:Button ID="Button03" runat="server" OnClick="Button03_Click" Text="3" />
+        <asp:Button ID="Button02" runat="server" OnClick="Button02_Click" Text="2" />
+        <asp:Button ID="Button01" runat="server" OnClick="Button01_Click" Text="1" />
+        <asp:Button ID="Button0" runat="server" OnClick="Button0_Click" Text="0" />
+        <asp:Button ID="ButtonErase" runat="server" OnClick="ButtonErase_Click" Text="&lt;=" />
+    </form>
+</body>
+</html>
---
+++ WebCalculator/Calculator.aspx.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebCalculator
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        string firstNumber = "";
+        string secondNumber = "";
+        string totalString = "";
+        string signOperation = "";
+        bool fraction = false;
+        double result = 0;
+
+        CalcResult.CalServiceSoapClient calculator = new CalcResult.CalServiceSoapClient();
+
+        public void Reload(List<string> array)
+        {
+            this.firstNumber = array[0];
+            this.signOperation = array[1];
+            this.secondNumber = array[2];
+            this.totalString = array[3];
+            this.fraction = bool.Parse(array[4]);
+            this.result = Convert.ToDouble(array[5]);
+
+            if (Session["firstNumber"] != null)
+            {
+                Session["firstNumber"] = firstNumber;
+            }
+
+            if (Session["signOperation"] != null)
+            {
+                Session["signOperation"] = signOperation;
+            }
+
+            if (Session["secondNumber"] != null)
+            {
+                Session["secondNumber"] = secondNumber;
+            }
+
+            if (Session["totalString"] != null)
+            {
+                Session["totalString"] = totalString;
+            }
+
+            if (Session["fraction"] != null)
+            {
+                Session["fraction"] = fraction.ToString();
+            }
+
+            if (Session["result"] != null)
+            {
+                Session["result"] = result.ToString();
+            }
+        }
+
+        public List<string> Refreshed()
+        {
+            if (Session["firstNumber"] != null)
+            {
+                this.firstNumber = Session["firstNumber"].ToString();
+            }
+
+            if (Session["signOperation"] != null)
+            {
+                this.signOperation = Session["signOperation"].ToString();
+            }
+
+            if (Session["secondNumber"] != null)
+            {
+                this.secondNumber = Session["secondNumber"].ToString();
+            }
+
+            if (Session["totalString"] != null)
+            {
+                this.totalString = Session["totalString"].ToString();
+            }
+
+            if (Session["fraction"] != null)
+            {
+                this.fraction = bool.Parse(Session["fraction"].ToString());
+            }
+
+            if (Session["result"] != null)
+            {
+                this.result = Convert.ToDouble(Session["result"].ToString());
+            }
+
+            //As List(Of String)
+            List<string> cal_vars = new List<string>();
+            cal_vars.Add(firstNumber);
+            cal_vars.Add(signOperation);
+            cal_vars.Add(secondNumber);
+            cal_vars.Add(totalString);
+            cal_vars.Add(fraction.ToString());
+            cal_vars.Add(result.ToString());
+            //return cal_vars.ToArray();
+            return cal_vars;
+        }
+
+        public void ResetAll()
+        {
+            this.firstNumber = "";
+            this.secondNumber = "";
+            this.totalString = "";
+            this.signOperation = "";
+            this.resultBox.Text = "";
+            this.fraction = false;
+            this.result = 0;
+        }
+
+        public void buttonClicked(int digit)
+        {
+            List<string> cal_memory = Refreshed();
+            List<string> calculation = calculator.ButtonPressed(digit, firstNumber, signOperation, secondNumber, totalString, fraction, result);
+            Reload(calculation);
+            resultBox.Text = totalString;
+        }
+
+        public void signClicked(string sign)
+        {
+            List<string> cal_memory = Refreshed();
+            List<string> calculation = calculator.SignPressed(sign, firstNumber, signOperation, secondNumber, totalString, fraction, result);
+            Reload(calculation);
+            resultBox.Text = totalString;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            calculator.Initialize();
+
+            if(Session["firstNumber"] == null)
+            {
+                Session["firstNumber"] = "";
+            }
+
+            if (Session["signOperation"] == null)
+            {
+                Session["signOperation"] = "";
+            }
+
+            if (Session["secondNumber"] == null)
+            {
+                Session["secondNumber"] = "";
+            }
+
+            if (Session["totalString"] == null)
+            {
+                Session["totalString"] = "";
+            }
+
+            if (Session["fraction"] == null)
+            {
+                Session["fraction"] = "false";
+            }
+
+            if (Session["result"] == null)
+            {
+                Session["result"] = "0";
+            }
+        }
+
+        protected void Button0_Click(object sender, EventArgs e)
+        {
+            buttonClicked(0);
+        }
+
+        protected void Button01_Click(object sender, EventArgs e)
+        {
+            buttonClicked(1);
+        }
+
+        protected void Button02_Click(object sender, EventArgs e)
+        {
+            buttonClicked(2);
+        }
+
+        protected void Button03_Click(object sender, EventArgs e)
+        {
+            buttonClicked(3);
+        }
+
+        protected void Button04_Click(object sender, EventArgs e)
+        {
+            buttonClicked(4);
+        }
+
+        protected void Button05_Click(object sender, EventArgs e)
+        {
+            buttonClicked(5);
+        }
+
+        protected void Button06_Click(object sender, EventArgs e)
+        {
+            buttonClicked(6);
+        }
+
+        protected void Button07_Click(object sender, EventArgs e)
+        {
+            buttonClicked(7);
+        }
+
+        protected void Button08_Click(object sender, EventArgs e)
+        {
+            buttonClicked(8);
+        }
+
+        protected void Button09_Click(object sender, EventArgs e)
+        {
+            buttonClicked(9);
+        }
+
+        protected void ButtonClear_Click(object sender, EventArgs e)
+        {
+            Session["firstNumber"] = "";
+            Session["signOperation"] = "";
+            Session["secondNumber"] = "";
+            Session["totalString"] = "";
+            Session["fraction"] = "false";
+            Session["result"] = "0";
+
+            ResetAll();
+        }
+
+        protected void ButtonSum_Click(object sender, EventArgs e)
+        {
+            signClicked("+");
+        }
+
+        protected void ButtonSub_Click(object sender, EventArgs e)
+        {
+            signClicked("-");
+        }
+
+        protected void ButtonMult_Click(object sender, EventArgs e)
+        {
+            signClicked("x");
+        }
+
+        protected void ButtonDiv_Click(object sender, EventArgs e)
+        {
+            signClicked("/");
+        }
+
+        protected void ButtonPer_Click(object sender, EventArgs e)
+        {
+            signClicked("%");
+        }
+
+        protected void ButtonSqr_Click(object sender, EventArgs e)
+        {
+            List<string> cal_memory = Refreshed();
+
+            if (signOperation == "")
+            {
+                if (firstNumber != "")
+                {
+                    firstNumber = (Convert.ToDouble(firstNumber) * Convert.ToDouble(firstNumber)).ToString();
+                    totalString = firstNumber;
+                    Session["firstNumber"] = firstNumber;
+                }
+            }
+            else
+            {
+                if (secondNumber != "")
+                {
+                    secondNumber = (Convert.ToDouble(secondNumber) * Convert.ToDouble(secondNumber)).ToString();
+                    totalString = firstNumber + " " + signOperation + " " + secondNumber;
+                    Session["secondNumber"] = secondNumber;
+                }
+            }
+
+            resultBox.Text = totalString;
+            Session["totalString"] = totalString;
+        }
+
+        protected void ButtonRoot_Click(object sender, EventArgs e)
+        {
+            List<string> cal_memory = Refreshed();
+
+            if (signOperation == "")
+            {
+                if (firstNumber != "")
+                {
+                    firstNumber = Math.Sqrt((Convert.ToDouble(firstNumber))).ToString();
+                    totalString = firstNumber;
+                    Session["firstNumber"] = firstNumber;
+                }
+            }
+            else
+            {
+                if (secondNumber != "")
+                {
+                    secondNumber = Math.Sqrt((Convert.ToDouble(secondNumber))).ToString();
+                    totalString = firstNumber + " " + signOperation + " " + secondNumber;
+                    Session["secondNumber"] = secondNumber;
+                }
+            }
+
+            resultBox.Text = totalString;
+            Session["totalString"] = totalString;
+        }
+
+        protected void ButtonDott_Click(object sender, EventArgs e)
+        {
+            List<string> cal_memory = Refreshed();
+
+            if (fraction == false)
+            {
+                if (signOperation == "")
+                {
+                    if (firstNumber == "")
+                    {
+                        firstNumber += "0.";
+                        Session["firstNumber"] = firstNumber;
+                    }
+                    else
+                    {
+                        firstNumber += ".";
+                        Session["firstNumber"] = firstNumber;
+                    }
+
+                }
+                else
+                {
+                    if (secondNumber == "")
+                    {
+                        secondNumber += "0.";
+                        Session["secondNumber"] = secondNumber;
+                    }
+                    else
+                    {
+                        secondNumber += ".";
+                        Session["secondNumber"] = secondNumber;
+                    }
+                }
+
+                fraction = true;
+                totalString += ".";
+                resultBox.Text = totalString;
+
+                Session["fraction"] = fraction.ToString();
+                Session["totalString"] = totalString;
+            }
+        }
+
+        protected void ButtonEqual_Click(object sender, EventArgs e)
+        {
+            List<string> cal_memory = Refreshed();
+            List<string> calculation = calculator.Calculation(firstNumber, signOperation, secondNumber, totalString, fraction, result);
+            Reload(calculation);
+            resultBox.Text = totalString;
+            firstNumber = result.ToString();
+            signOperation = "";
+            Session["firstNumber"] = firstNumber;
+            Session["signOperation"] = "";
+        }
+
+        protected void ButtonErase_Click(object sender, EventArgs e)
+        {
+            List<string> cal_memory = Refreshed();
+
+            if (secondNumber != "")
+            {
+                secondNumber = secondNumber.Remove(secondNumber.Length - 1, 1);
+                Session["secondNumber"] = secondNumber;
+            }
+            else if (signOperation != "")
+            {
+                signOperation = "";
+                Session["signOperation"] = "";
+            }
+            else if (firstNumber != "")
+            {
+                firstNumber = firstNumber.Remove(firstNumber.Length - 1, 1);
+                Session["firstNumber"] = firstNumber;
+            }
+
+            totalString = firstNumber + " " + signOperation + " " + secondNumber ;
+            resultBox.Text = totalString;
+            Session["totalString"] = totalString;
+        }
+    }
+}
